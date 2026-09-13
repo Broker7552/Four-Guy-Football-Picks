5530d2d05d712bdc5f3a2dc5e3c4527bc6168aa8
Broker7552 <scottkelly7552@gmail.com>
2026-09-13 16:14:20 -0400

    Bust cache for live ATS display

--- college-revision-time.js
+++ college-revision-time.js
@@ -19,7 +19,7 @@
 
 if(typeof document!=='undefined'&&!document.querySelector('script[data-live-week]')){
   const s=document.createElement('script');
-  s.src='live-week.js?v=standings-3';
+  s.src='live-week.js?v=ats-margin-1';
   s.dataset.liveWeek='1';
   document.head.appendChild(s);
 }
